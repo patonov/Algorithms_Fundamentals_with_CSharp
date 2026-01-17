fe6afb9e549930aed0d09be02a0aa9e09523201b
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2026-01-17 20:32:47 +0200

    Add files via upload

---
+++ AlgorithmsLab/AlgorithmsLab/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AlgorithmsLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the position of the Fibonacci number you want:");
+            int position = int.Parse(Console.ReadLine()!);
+
+            //int result = Fibonacci(position);
+            //Console.WriteLine($"Fibonacci number at position {position} is: {result}");
+            PrintFibonacciUpTo(position);
+        }
+        
+        private static int Fibonacci(int n)
+        {
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private static void PrintFibonacciUpTo(int position)
+        {
+            for (int i = 0; i <= position; i++)
+            {
+                Console.WriteLine($"Fibonacci number at position {i} is: {Fibonacci(i)}");
+            }
+        }
+    }
+}
